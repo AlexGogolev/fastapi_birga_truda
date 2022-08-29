173eef61df1b756e24b0f6504f352b4aa380d36f
alex <inkeeper@inbox.ru>
2022-08-29 23:08:06 +0500

    начали заполнять config.py для работы с перем. окружения

--- config.py
+++ config.py
@@ -1,2 +1,12 @@
 """тут храняться переменные, связанные с конфигурацией приложения"""
 
+from starlette.config import Config  # класс Config нужен для работы с переменными окружения ОС
+
+config = Config(".env")  # env_file
+
+DATABASE_URL = config("EE_DATABASE")  # EE_DATABASE - переменная окружения
+
+
+
+
+
