b74f100cd0b54ea69cc3e2388b9b680b903e8714
alex <inkeeper@inbox.ru>
2022-08-29 23:58:19 +0500

    added подключение к БД при старте и заверше прилож

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 import uvicorn
+from db.base import database
 
 
 app = FastAPI()
@@ -7,6 +8,17 @@ app = FastAPI()
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+
+# приложение должно подключаться к БД на самом старте
+@app.on_event("startapp")  # это выполняется во время старта приложения
+async def startapp():
+    await database.connect()
+    
+    
+@app.on_event("shutdown")
+async def shutdown():
+    await database.disconnect()
 
 
 if __name__ == '__main__':
